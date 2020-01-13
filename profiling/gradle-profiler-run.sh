68f5920556fc653559fbddc5d67f8a0b9de78713
Christobal <geekarist@users.noreply.github.com>
2020-01-13 08:33:11 +0100

    Remove profiling/

--- profiling/gradle-profiler-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-gradle-profiler \
-  --profile buildscan \
-  --project-dir ../ \
-  --output-dir tmp/ \
-  --gradle-user-home tmp/gradle-user-home/ \
-  --scenario-file gradle-profiler-scenarios.conf
